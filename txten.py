274da37dbf7fbeb84c4f89aab738a6fdf9ba6534
WhitePaperKat <41842361+lostfile1@users.noreply.github.com>
2020-11-16 16:45:40 -0500

    Update txten.py
    removed un needed code

--- txten.py
+++ txten.py
@@ -5,11 +5,9 @@ import sys
 
 
 if sys.argv[2] == 'en':
-  encodedBytes = base64.b64encode(sys.argv[1].encode("utf-8"))
-  encodedStr = str(encodedBytes, "utf-8")
-  print(encodedStr)
+  encodedBytes = base64.b64encode(sys.argv[1])
+  print(encodedBytes)
 
 elif sys.argv[2] == 'dc':
   decodedBytes = base64.b64decode(sys.argv[1])
-  decodedStr = str(decodedBytes, "utf-8")
-  print(decodedStr)   
+  print(decodedBytes)   
