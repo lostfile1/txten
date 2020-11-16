eb22c3f78c1b0b6f9d7b555056203c5b4b9e08e7
WhitePaperKat <41842361+lostfile1@users.noreply.github.com>
2020-11-16 17:06:03 -0500

    Update txten.py

--- txten.py
+++ txten.py
@@ -2,11 +2,11 @@ import base64
 import sys
 
 if sys.argv[1] == 'en':
-  data = base64.b64encode(sys.argv[2])
+  data = base64.b64encode(sys.argv[2].encode("ascii"))
   print(data)
  
 elif sys.argv[1] == 'dc':
-   data = base64.b64decode(sys.argv[2])
+   data = base64.b64decode(sys.argv[2].encode("ascii"))
    print(data)
 
 
