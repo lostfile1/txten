a8da47c9723db78cebfef7bc1a090090efa35533
WhitePaperKat <41842361+lostfile1@users.noreply.github.com>
2020-11-16 17:24:28 -0500

    Update txten.py

--- txten.py
+++ txten.py
@@ -1,9 +1,10 @@
 import base64
 import sys
 import os
+data = sys.argv[1]
 
-print(base64.b64encode(sys.argv[1].encode("utf-8")))
-  
+# Standard Base64 Encoding
+encodedBytes = base64.b64encode(data.encode("utf-8"))
+encodedStr = str(encodedBytes, "utf-8")
 
-
-
+print(encodedStr)
