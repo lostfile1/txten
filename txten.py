9cd526d497405937fbd6ecd431924d56a89b206d
WhitePaperKat <41842361+lostfile1@users.noreply.github.com>
2020-12-30 20:51:19 -0500

    Update txten.py

--- txten.py
+++ txten.py
@@ -6,14 +6,14 @@ import base64
 if sys.argv[2] == 'en':
   print(base64.b64encode(sys.argv[1]))
 
-elif sys.argv[2] == 'de': 
+if sys.argv[2] == 'de': 
   print(base64.b64decode(sys.argv[1]))
-
-elif sys.argv[2] == 'credit':
+  
+if sys.argv[2] == 'credit':
   print('script made by whitepaperkat')
   print('donate : bc1qyvrvxygaw4dqmwu3sg0gsl79uwyh6u2p99vcr2\n')
   
-elif sys.argv[2] == 'h':
+if sys.argv[2] == 'h':
   print('Commands\n')
   print('en Encode\n')
   print('de Decode\n')
